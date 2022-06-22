27439067712d6bccd3656b7d0c4929a7cf7a4383
Ohmry <o.ohmry@gmail.com>
2022-06-22 23:14:08 +0900

    initailize

---
+++ src/main/java/io/ohmry/dddjpaexample/DddJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package io.ohmry.dddjpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddJpaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ohmry/dddjpaexample/DddJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ohmry.dddjpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddJpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
